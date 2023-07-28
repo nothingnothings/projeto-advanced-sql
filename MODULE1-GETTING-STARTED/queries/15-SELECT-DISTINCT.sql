20f091422c1d0bc43089e9b29cebc799ac293d5a
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2023-07-28 12:29:47 -0300

    finished lesson 15

---
+++ MODULE1-GETTING-STARTED/queries/15-SELECT-DISTINCT.sql
@@ -0,0 +1,160 @@
+
+
+
+VEREMOS 1 VARIATION DO SELECT STATEMENT...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+É O SELECT DISTINCT,
+
+
+EM VEZ DE SÓ SELECT...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--> COM ELE, PODEMOS SELECIONAR OS VALUES DISTINTOS 
+
+
+EM 1 TABLE...
+
+
+
+
+
+
+
+
+ESSE STATEMENT É USADO ASSIM:
+
+
+
+
+
+
+
+
+
+
+
+
+
+SELECT DISTINCT 
+<column_name_1>
+FROM table_name;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+COM ISSO, SELECIONAMOS QUAL COLUMN GOSTARÍAMOS DE TER 
+
+OS DISTINCT VALUES...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- PODEMOS USAR O SELECT DISTINCT 
+
+
+COM VÁRIAS 
+
+COLUMNS, TAMBÉM...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--. TEMOS ESTE EXEMPLLO:
+
+
+
+
+
+
+
+
+
+
+
+
+SELECT DISTINCT 
+first_name
+FROM actor
+ORDER BY first_name;
+
+
+
+
+
+------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
