5cf6818f8cd5dc33788474f453cce8107fdfa758
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2023-07-28 14:31:44 -0300

    finished lesson 9

---
+++ MODULE2-FILTERING/queries/9-WHERE-E-AND-OR-CHALLENGE-SOLUTION.sql
@@ -0,0 +1,57 @@
+
+
+
+
+
+
+FICOU TIPO ASSIM:
+
+
+
+
+
+
+
+
+
+
+
+SELECT 
+*
+FROM PAYMENT 
+WHERE 
+(
+CUSTOMER_ID = 322 OR 
+CUSTOMER_ID = 346 OR 
+CUSTOMER_ID = 354)
+AND 
+(amount < 2 OR AMOUNT > 10)
+ORDER BY CUSTOMER_ID, AMOUNT DESC;
+
+
+
+
+
+
+
+
+
+
+
+
+CERTO....
+
+
+
+
+
+
+
+
+
+
+
+ACERTEI...
+
+
+
