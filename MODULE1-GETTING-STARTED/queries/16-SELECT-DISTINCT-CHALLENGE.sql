38b1982d14d6336fd5780029f298af951bce5bdb
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2023-07-28 12:33:43 -0300

    Finished lesson 16

---
+++ MODULE1-GETTING-STARTED/queries/16-SELECT-DISTINCT-CHALLENGE.sql
@@ -0,0 +1,76 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+NESSA CHALLENGE, 1 PESSOA DA MARKETING TEAM 
+
+PEDE 
+
+SOBRE 
+
+
+"THE DIFFERENT PRICES THAT HAVE BEEN PAID"...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--> PARA DEIXAR AS COISAS MAIS FÁCEIS PARA ELES,
+
+VC DEVE ORDENAR OS PRICES,
+
+DE ALTOS PARA BAIXOS...
+
+
+
+
+
+
+
+
+
+
+----> deve ser na table "payments"...
+
+
+
+
+
+
+
+
+
+
+
+
+
+ACHO QUE A QUERY FICA TIPO:
+
+
+
+
+
+
+SELECT DISTINCT amount FROM payment
+ORDER BY amount DESC;
+
+
+
