b70cc5e390511be7f3c14cd6425ab7df57b53da7
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2023-07-28 16:13:21 -0300

    finished lesson 16

---
+++ MODULE2-FILTERING/queries/16-IN-CHALLENGE-SOLUTION.sql
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+O PROFESSOR COMECA ASSIM:
+
+
+
+
+
+
+
+
+SELECT * FROM payment
+WHERE customer_id IN (12, 25,67, 93, 124, 234)
+AND amount IN (4.99, 7.99, 9.99)
+AND payment_date BETWEEN '2020-01-01' AND '2020-02-01';
+
+
+
+
+
+
+
+
+
+
+
+
