db9b6caad65a641bf2abe193b83e6df26e2180fe
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2023-07-28 13:12:31 -0300

    Finished lesson 2

---
+++ MODULE2-FILTERING/queries/2-WHERE-CHALLENGE.sql
@@ -0,0 +1,97 @@
+
+
+
+
+
+
+
+
+
+
+
+
+A CHALLENGE É ESTA:
+
+
+
+
+
+
+
+
+
+1) HOW MANY PAYMENTS WERE MADE BY THE CUSTOMER 
+
+WITH CUSTOMER_ID = 100;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--> PARA ISSO, ESCREVO ASSIM:
+
+
+
+
+
+
+SELECT * FROM payment 
+WHERE CUSTOMER_ID = 100;
+
+
+
+
+
+
+-----------------------
+
+
+
+
+
+
+
+
+
+A SEGUNDA TASK É ASSIM:
+
+
+
+
+
+
+
+
+2) WHAT IS THE LAST NAME OF OUR CUSTOMER
+WITH FIRST NAME 'ERICA'...
+
+
+
+
+
+
+
+
+
+
+
+
+
+ESCREVO ASSIM:
+
+
+
+
+SELECT * FROM customer 
+WHERE first_name='ERICA';
