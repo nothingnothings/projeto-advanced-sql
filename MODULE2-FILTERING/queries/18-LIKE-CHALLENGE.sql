145786009ec449311ea352755355aff7e4df5cc9
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2023-07-28 17:19:41 -0300

    finished lesson 18

---
+++ MODULE2-FILTERING/queries/18-LIKE-CHALLENGE.sql
@@ -0,0 +1,50 @@
+
+
+NOSSA CHALLENGE É ESTA:
+
+
+
+
+
+
+
+
+
+
+
+HOW MANY MOVIES ARE THERE THAT CONTAIN THE 
+
+'Documentary' word in the description..
+
+
+
+
+
+
+
+
+
+
+PARA ISSO, ACHO QUE MINHA QUERY FICARÁ ASSIM:
+
+
+
+
+
+
+
+
+SELECT
+COUNT(*)
+FROM film 
+WHERE description LIKE '%Documentary%';
+
+
+
+
+
+
+
+
+
+
