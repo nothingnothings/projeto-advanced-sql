246a515c90f2189e528093e5080be0771feaa5c4
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2023-07-31 14:56:27 -0300

    finished lesson 3

---
+++ MODULE4-FUNCTIONS/queries/3-LENGTH-LOWER-UPPER-CHALLENGE.sql
@@ -0,0 +1,94 @@
+
+
+
+
+
+
+
+
+
+
+TEMOS ESTA CHALLENGE:
+
+
+
+
+
+
+
+
+
+
+--> PROBLEMA NO SISTEMA DE EMAILS 
+
+DA COMPANHIA...
+
+
+
+
+
+
+
+PROBLEMA:
+
+
+1) SE O PRIMEIRO OU ÚLTIMO NOME 
+
+
+DE 1 
+
+PESSOA 
+TEM MAIS DE 10 CARACTERES,
+
+ELE NAO É ENVIADO...
+
+
+
+
+
+
+
+2) SUA TAREFA É ENCONTRAR ESSES CUSTOMERS 
+
+
+E __ OUTPUTTAR_ 1 LISTA 
+
+
+DESSES FIRST _e LAST NAMES,
+
+
+TODOS EM LOWER CASE....
+
+
+
+
+
+
+
+
+-> PARA ISSO, ACHO QUE PODEMOS RODAR UMA QUERY ASSIM:
+
+
+
+
+
+
+SELECT 
+LOWER(first_name),
+LOWER(last_name)
+FROM customer
+WHERE LENGTH(first_name) > 10 OR 
+      LENGTH(last_name) > 10;
+
+
+
+
+
+
+
+
+
+
+OK... DEU CERTO,
+
+ENCONTREI 8 PESSOAS....
