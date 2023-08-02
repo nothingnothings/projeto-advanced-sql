17cc5a1397b5481674f4be58e40bb408d167e6a3
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2023-08-02 13:53:21 -0300

    finished lesson 13

--- MODULE6-JOINS/queries/12-MULTIPLE-JOIN-CONDITIONS.sql
+++ MODULE6-JOINS/queries/12-MULTIPLE-JOIN-CONDITIONS.sql
@@ -839,3 +839,75 @@ DEVEMOS VER SE ISSO ESTÁ CERTO,
 
 NA PRÓXIMA AULA...
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+--> A SOLUCAO DO PROFESSOR FOI ASSIM:
+
+
+
+
+
+
+
+SELECT 
+seat_no,
+ROUND(AVG(amount), 2)
+
+FROM boarding_passes AS bp
+LEFT JOIN ticket_flights AS tf
+ON bp.ticket_no = tf.ticket_no 
+AND bp.flight_id = tf.flight_id
+GROUP BY seat_no
+ORDER BY 2;
+
+
+
+
+
+
+
+
+
+
+
+
+
+CERTO... ISSO FUNCIONOU....
+
+
+
+
+
+
+
+
+OCASIONALMENTE ISSO É NECESSÁRIO,
+
+QUANOD 
+
+PRECISAMOS USAR MÚLTIPLAS COLUMNS 
+
+
+EM 1 JOIN,
+
+
+
+MÚLTIPLAS COLUMNS FORMANDO 1 PRIMARY KEY (
+
+    composite keys...
+)
+
+
+
+
