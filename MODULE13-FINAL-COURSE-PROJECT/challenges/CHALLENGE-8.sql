3f6911ea1326e326417efd7e164eb84c575735bc
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2023-08-07 18:24:49 -0300

    finished challenge 8

--- MODULE13-FINAL-COURSE-PROJECT/challenges/CHALLENGE-8.sql
+++ MODULE13-FINAL-COURSE-PROJECT/challenges/CHALLENGE-8.sql
@@ -37,3 +37,48 @@ salary,
 ROUND(AVG(salary) OVER(PARTITION BY position_title ORDER BY emp_id), 2) AS avg_salary_for_this_position
 FROM employees
 ORDER BY emp_id;
+
+
+
+
+
+
+
+
+-- Task 8.2
+
+-- Difficulty: Advanced to Pro
+
+
+
+-- How many people earn less than the avg_position_salary?
+
+-- Write a query that answers that question.
+
+-- Ideally, the output just shows that number directly.
+
+
+
+QUERY:
+
+
+
+SELECT 
+COUNT(*)
+FROM (
+SELECT
+emp_id,
+salary,
+ROUND(AVG(salary) OVER(PARTITION BY position_title), 2) AS avg_salary_for_this_position
+FROM employees
+GROUP BY emp_id 
+) AS result_set
+WHERE salary < avg_salary_for_this_position;
+
+
+
+
+
+Answer:
+
+9
